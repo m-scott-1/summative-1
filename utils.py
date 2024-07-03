c52fbf15f1c6022c2bd7e7d970f855612af6a408
m-scott-1 <ms2117a@students.nulondon.ac.uk>
2024-07-04 00:27:41 +0100

    changing multiple files

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
-"""User defined utility functions. Used in app.py and for testing"""
+"""
+User defined utility functions. Used in app.py and for testing
+
+Module level testing:
+
+>>> load_model("en_core_web_sm")
+spacy.lang.en.English
+
+"""
 
 import spacy
 import streamlit as st
@@ -16,4 +24,29 @@ def load_models(model):
     assert isinstance(nlp, spacy.lang.en.English), "Language model is not an English variant"
 
     return nlp
+# end
+
+def annotation_wrangler(doc, model):
+    """
+    Create data structure whilst annotating text to conform to annotated_text input
+    """
+
+    colours = ["#8ef", "#faa", "#afa", "#fea", "#faf", "#baf", "#f83", "#0aa"]
+    a_counter = 0
+    annotate = []
+    if model == "word":
+        for token in doc:
+            annotate.append((str(token), "", colours[a_counter]))
+            a_counter +=1
+            if a_counter == 7:
+                a_counter = 0
+
+    elif model == "sent":
+        for token in doc.sents:
+            annotate.append((str(token), "", colours[a_counter]))
+            a_counter +=1
+            if a_counter == 7:
+                a_counter = 0
+    
+    return annotate
 # end
