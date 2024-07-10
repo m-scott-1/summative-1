5a3514cbc7d49963422d106125e426870046ee7c
m-scott-1 <ms2117a@students.nulondon.ac.uk>
2024-07-10 08:11:24 +0100

    updating modularit of app

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ In terminal of working directory:
 python -m streamlit run app.py
 this will open a network url where edits can be
 seen in real time on save and refresh
+ctrl + c to stop a local streamlit instance
 """
 
-from spacy_streamlit import visualize_ner, visualize_parser
+from spacy_streamlit import visualize_ner
 import streamlit as st
 from annotated_text import annotated_text
-import pandas as pd
-import spacy
 import utils
 
 DEFAULT_TEXT = """CELLULAR COMMUNICATIONS INC. sold 1,550,000 common shares at $21.75 each yesterday, \
@@ -24,7 +23,7 @@ st.title("Learn Natural Language Processing Concepts")
 
 model_selection = st.sidebar.selectbox("Choose a model", MODELS, 0)
 
-st.sidebar.markdown(f"Selected model: **`{model_selection}`**")
+st.sidebar.markdown(f"Selected SpaCy model: **`{model_selection}`**")
 
 nlp = utils.load_models(model_selection)
 
@@ -32,31 +31,40 @@ input_text = st.text_area("Enter text to be analysed", DEFAULT_TEXT, height = 10
 
 doc = nlp(input_text)
 
-tab1, tab2, tab3 = st.tabs(["Tokenisation", "Part of Speech Tagging", "Dependency Tree Parsing"])
+tab1, tab2, = st.tabs(["Tokenisation", "Part of Speech Tagging"])
 
 with tab1:
+
     st.subheader("Tokenisation")
-    st.markdown("#### SpaCy")
-    spacy_choice = st.radio("Select a model:", ["word", "sent"], 
-                            captions= ["Word tokenisation", "Sentence tokenisation"],
-                            horizontal=True)
-    spacy_annotate = utils.annotation_wrangler(doc, model = spacy_choice)
-    annotated_text(spacy_annotate)
-    st.markdown("#### NLTK")
+
+    st.markdown("#### SpaCy models")
+
+    spacy_radio = st.radio("Select an tokeniser:", ["spacy_word", "spacy_sent"],
+                        captions= ["SpaCy word tokenisation", "SpaCy sentence tokenisation"],
+                        horizontal=True,
+                        help= "Choose between different SpaCy word or sentence tokenisers.")
+
+    first_annotate = utils.token_annotator(doc, model = spacy_radio)
+
+    annotated_text(first_annotate)
+
+    st.markdown("#### NLTK models")
+
+    nltk_radio = st.radio("Select an tokeniser:", ["nltk_whitespace", "nltk_treebank", "nltk_twitter", "nltk_punkt"],
+                        captions= ["NLTK whitespace tokeniser", "NLTK treebank tokeniser", "NLTK twitter tokeniser",
+                                   "NLTK Punkt sentence tokeniser"],
+                        horizontal=True,
+                        help= "Choose between different NLTK word or sentence tokenisers.")
+
+    second_annotate = utils.token_annotator(doc, model = nltk_radio)
+
+    annotated_text(second_annotate)
 
 with tab2:
+
     st.subheader("Part of Speech Tagging")
-    df = pd.DataFrame({
-        "Text": [token.text for token in doc],
-        "Lemma": [token.lemma_ for token in doc],
-        "POS": [token.pos_ for token in doc],
-        "POS explained": [spacy.explain(token.pos_) for token in doc],
-        "Dependency": [token.dep_ for token in doc]
-    })
+    df = utils.pos_table(doc=doc)
     st.dataframe(df, hide_index=True, use_container_width=True)
+
     st.subheader("Named Entity Recognition")
     visualize_ner(doc, labels=nlp.get_pipe("ner").labels, title=None, show_table=False)
-
-with tab3:
-    st.subheader("Dependency Parsing")
-    visualize_parser(doc, title=None)
